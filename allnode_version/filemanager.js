a81c98e4dab8eb4fb8475d15608fdd3ff50b33fa
Name <kasusaland@gmail.com>
2025-01-11 17:53:42 +0800

    update this

--- allnode_version/filemanager.js
+++ allnode_version/filemanager.js
@@ -19,6 +19,18 @@ function createFile(filename) {
 }
 
 function writeFile(filename, content) {
+  // 如果文件夹不存在，则创建它
+  const path = require('path');
+  const folderPath = path.dirname(filename);
+  if (!fs.existsSync(folderPath)) {
+    try {
+      fs.mkdirSync(folderPath, { recursive: true });
+      console.log(`文件夹 ${folderPath} 已创建`);
+    } catch (err) {
+      console.error(`创建文件夹 ${folderPath} 时出错: ${err.message}`);
+      throw err; // 或者根据需要处理错误
+    }
+  }
   fs.writeFileSync(filename, content);
   console.log(`内容已成功写入文件 ${filename}`);
 }
